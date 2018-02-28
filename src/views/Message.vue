2ed325a4c85e943143240ee72a17188f211531c9
gzl <15293169183@163.com>
2018-02-28 21:04:54 +0800

    add

--- src/views/Message.vue
+++ src/views/Message.vue
@@ -3,10 +3,15 @@
     <ul>
       <li v-for="(m, index) in messages" :key="index">
         <router-link :to="`/home/message/detail/${m.id}`">{{m.title}}</router-link>
+        <button @click="push(m.id)">push查看</button>
+        <button @click="replace(m.id)">replace查看</button>
       </li>
+
     </ul>
+
     <hr>
     <router-view></router-view>
+    <button @click="handleReturn">返回</button>
   </div>
 </template>
 
@@ -15,6 +20,17 @@
     data(){
       return{
         messages:[]
+      }
+    },
+    methods:{
+      push(id){
+        this.$router.push(`/home/message/detail/${id}`)
+      },
+      replace(id){
+        this.$router.replace(`/home/message/detail/${id}`)
+      },
+      handleReturn(){
+        this.$router.back()
       }
     },
     mounted(){
